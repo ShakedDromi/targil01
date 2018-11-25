333a89b0e0c858d512fa28f1a6274c8422e02240
ShskedDromi <sd5308@bs.amalnet.k12.il>
2018-11-25 22:29:42 +0200

    first

---
+++ app/src/main/java/com/example/shakeddromi/targil01/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.shakeddromi.targil01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn=(Button)findViewById(R.id.button);
+    }
+
+    public void click(View view) {
+        btn.setText("Oh yeah, I've been clicked!");
+    }
+}
